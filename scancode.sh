86e1d22ebd0de79f174df5c27fbd24bb75a0d96a
Vinod R <vinod.gowda@tarams.com>
2020-05-27 22:11:01 +0530

    update scancode.sh

--- scancode.sh
+++ scancode.sh
@@ -4,4 +4,4 @@ virtualenv -p python3 scancode
 source scancode/bin/activate
 pip3 install scancode-toolkit
 pip3 install -r api/requirements.txt
-scancode -clp --html compliance-report.html scancode/lib/python3.6/site-packages/
+scancode -clp --html compliance-report.html scancode/lib/python3.6/site-packages/requests
